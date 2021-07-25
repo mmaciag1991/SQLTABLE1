c275532efc83b3683454ff539b2b66144f45cc59
Mateusz Maciąg <46632727+mmaciag1991@users.noreply.github.com>
2021-07-25 10:24:51 +0200

    pdate

--- Controls/MainWindow.xaml.cs
+++ Controls/MainWindow.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.IO;
 using System.Windows;
 using System.Windows.Media.Imaging;
+using SQLTABLE.ObservableLists;
 
 namespace SQLTABLE
 {
@@ -13,6 +16,8 @@ namespace SQLTABLE
     /// </summary>
     public partial class MainWindow
     {
+        private ObservableCollection<User> _users = SQL_Module.LoadUsersData();
+        
         public MainWindow()
         {
             
@@ -26,9 +31,18 @@ namespace SQLTABLE
             
             DataTable dt = new DataTable("Urser");
 
-            SQL_Module.getUsersData().Fill(dt);
+            //SQL_Module.getUsersData().Fill(dt);
+            
+            
  
-            grdEmployee.ItemsSource = dt.DefaultView;
+            grdEmployee.ItemsSource = _users;
+
+
+            _users.CollectionChanged += new System.Collections.Specialized.NotifyCollectionChangedEventHandler(collectionChangedMethod);
+            foreach (var user in _users)
+            {
+                Console.WriteLine("Id: "+user.Id+"  UserName: "+user.UserName+"  Password: "+user.Password+"  Adress: "+user.Adress);
+            }
             
         }
         private void btnExit_Click(object sender, RoutedEventArgs e)
@@ -44,6 +58,42 @@ namespace SQLTABLE
             this.Close();
         }
         
+        private void collectionChangedMethod(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            //different kind of changes that may have occurred in collection
+            if(e.Action == NotifyCollectionChangedAction.Add)
+            {
+                foreach (var item in e.NewItems)
+                {
+                    Console.WriteLine(((User)item).Password);
+                }
+               //Console.WriteLine(e.NewItems);
+            }
+            if (e.Action == NotifyCollectionChangedAction.Replace)
+            {
+                foreach (var item in e.NewItems)
+                {
+                    Console.WriteLine(((User)item).Password);
+                }
+            }
+            if (e.Action == NotifyCollectionChangedAction.Remove)
+            {
+                foreach (var item in e.NewItems)
+                {
+                    Console.WriteLine(((User)item).Password);
+                }
+            }
+            if (e.Action == NotifyCollectionChangedAction.Move)
+            {
+                foreach (var item in e.NewItems)
+                {
+                    Console.WriteLine(((User)item).Password);
+                }
+            }
+        }
+        
     }
+    
+    
   
 }
